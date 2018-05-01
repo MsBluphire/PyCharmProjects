b5b7a79a1dbb49edb2c223e4675118af8f899ecd
Denise <williamsde@outlook.com>
2018-05-01 17:46:35 -0400

    Completed Exercises 2-5 and 2-6

---
+++ python_work/famous_quote.py
@@ -0,0 +1,9 @@
+quote = '"Research is formalized curiosity. It is poking and prying with a purpose."'
+famous_person = "Zora Neale Hurston"
+
+print("Zora Neale Hurston is famous for saying 'Research is formalized curiosity. It is poking and prying with a purpose'")
+
+#Exercise 2-6
+
+message = famous_person + "is famous for stating " + quote
+print(message)
